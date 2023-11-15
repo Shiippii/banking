5f55eb094a1e5b3b192547ad7f494cb3e623425e
Shippi <nicholas_shippi@outlook.de>
2023-11-15 19:03:00 +0100

    adjusted lint script. linted project

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,11 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router"
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-      },
-    ],
+    path: "/",
+    component: () => import("@/views/MainDashboard.vue"),
+    name: "Dashboard",
   },
 ]
 
