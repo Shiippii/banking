acbb1c695023a549da7664cf342a1e177fd9b4d1
Shippi <nicholas_shippi@outlook.de>
2023-11-15 19:12:00 +0100

    added some eslint plugins & linted

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,18 @@ module.exports = {
   env: {
     node: true,
   },
+  plugins: ["sonarjs", "unicorn", "jsdoc"],
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "plugin:sonarjs/recommended",
+    "plugin:unicorn/recommended",
+    "plugin:jsdoc/recommended",
   ],
+  rules: {
+    "unicorn/prefer-module": "off",
+    "unicorn/filename-case": "off",
+    "unicorn/prefer-node-protocol": "off",
+    "unicorn/relative-url-style": "off",
+  },
 }
